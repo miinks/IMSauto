61ac9ffd4076ccf9668ec02daa0d37aab549bea1
Regie Agustin <124678791+miinks@users.noreply.github.com>
2025-06-27 00:04:20 -0500

    nav links

--- page2.js
+++ page2.js
@@ -4,3 +4,10 @@ const menuBtnIcon = menuBtn.querySelector("i");
 
 menuBtn.addEventListener("click", (e) => {
     navLinks.classList.toggle("open");
+
+     const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute(
+    "class",
+    isOpen ? "ri-close-line" : "ri-menu-3-line"
+  );
+});
